ee1d0fbe15b9cc10a1f13588c852f4d45b90828a
minhyuk <dufflaa@gmail.com>
2020-05-30 02:45:21 +0900

    Limitating Character Moving in Screen

--- game.py
+++ game.py
@@ -45,6 +45,16 @@ while running:
     character_x_pos += to_x
     character_y_pos += to_y
 
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+        
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
+
     screen.blit(background, (0, 0))
     screen.blit(character, (character_x_pos, character_y_pos))
     pygame.display.update()
