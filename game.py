f789936aefb8341058434a4b0adffadaeccf10c2
minhyuk <dufflaa@gmail.com>
2020-05-30 01:55:20 +0900

    Add background display and Quit Successfully

--- game.py
+++ game.py
@@ -2,18 +2,24 @@ import pygame
 
 pygame.init()
 
-print("hello world!")
-
 screen_width = 480
 screen_height = 640
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Z_Game")
 
+background = pygame.image.load("./background.png")
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        print(event)
 
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+
+pygame.display.quit()
 pygame.quit()
+exit()
