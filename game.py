ce5c4e105c9ed6f7b1f25d7f29913d3f9b12e5d7
minhyuk <dufflaa@gmail.com>
2020-05-30 02:12:39 +0900

    Add Character

--- game.py
+++ game.py
@@ -9,6 +9,12 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Z_Game")
 
 background = pygame.image.load("./background.png")
+character = pygame.image.load("./character.png")
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height
 
 running = True
 while running:
@@ -18,6 +24,7 @@ while running:
         print(event)
 
     screen.blit(background, (0, 0))
+    screen.blit(character, (character_x_pos, character_y_pos))
     pygame.display.update()
 
 pygame.display.quit()
